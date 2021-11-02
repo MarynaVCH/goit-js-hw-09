084961c03696e48be105ccd6e2429b2a910992da
MarynaVCH <83900520+MarynaVCH@users.noreply.github.com>
2021-11-02 20:57:41 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
 
+let timerId = null;
+
+refs.start.addEventListener('click', startChangeBackground);
+refs.stop.addEventListener('click', stopChangeBackground);
+
+function startChangeBackground() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.start.disabled = true;
+}
+
+function stopChangeBackground() {
+  clearInterval(timerId);
+  refs.start.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
